d254280dc8aaff8c128a4a872b135774a5f407d8
Bethany Morin <bmorin@industrydive.com>
2019-05-23 15:25:18 -0400

    Start adding pieces for modal form handling

--- rolllist/static/rolllist/js/rolllist.js
+++ rolllist/static/rolllist/js/rolllist.js
@@ -4,11 +4,32 @@ function set_dropdown_times(){
 	$('#id_end_time').val(start_val);
 }
 
+function bind_modal_open(){
+	$('a.openmodal').click(function(event){
+		event.preventDefault();
+		$('div#modal').show();
+	});
+}
+
+
+function bind_modal_close(){
+	$('a.closemodal').click(function(event){
+		event.preventDefault();
+		$('div#modal').hide();
+	});
+}
+
 $(document).ready(function(){
 	var on_additem_page = window.location.href.indexOf("additem") > 0;
 
 	if (on_additem_page) {
 		set_dropdown_times();
+		$.ajax(
+
+		)
 	}
+
+	bind_modal_open();
+	bind_modal_close();
 
 });
