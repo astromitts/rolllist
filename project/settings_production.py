53482297a6c3b21d7ca9d4d1eca95c9d769ec8fd
Bethany Morin <bmorin@industrydive.com>
2019-05-28 13:35:52 -0400

    Try postgres back-end?

--- project/settings_production.py
+++ project/settings_production.py
@@ -6,7 +6,12 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db-prod.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfklhffratb240',
+        'USER': 'angyjgxjtmqkhp',
+        'PASSWORD': 'ad970542e95ff459bb39232ae072b4d7e6f112f04c9949ac3706f61236650ee7',
+        'HOST': 'ec2-54-243-241-62.compute-1.amazonaws.com',
+        'PORT':'5432'
     }
 }
+# DATABASE_URL: postgres://angyjgxjtmqkhp:ad970542e95ff459bb39232ae072b4d7e6f112f04c9949ac3706f61236650ee7@ec2-54-243-241-62.compute-1.amazonaws.com:5432/dfklhffratb240
